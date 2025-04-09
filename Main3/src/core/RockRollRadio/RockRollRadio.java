15373c90188cb2d85be262bb6a64af9d4c79263a
manuelcastano <manuelcastano@uninorte.edu.co>
2025-04-09 13:57:16 -0500

    Csoasa

--- Main3/src/core/RockRollRadio/RockRollRadio.java
+++ Main3/src/core/RockRollRadio/RockRollRadio.java
@@ -4,10 +4,16 @@
  */
 package core.RockRollRadio;
 
+import main3.Locutor;
+
 /**
  *
  * @author glopr
  */
 public class RockRollRadio {
+
+    public void addLocutor(Locutor locutor) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
